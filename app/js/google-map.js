7af3bba7c82fa6bedfb64e77ef4154c771faa28b
Alex Rhind <mrsrhino2009@gmail.com>
2019-04-22 17:19:02 +0100

    Index v1

--- app/js/google-map.js
+++ app/js/google-map.js
@@ -1,38 +1,15 @@
-function init() {
+function initMap() {
 
-var pinLocation = new google.maps.LatLng(55.627154,-6.183849);
+	var myLatLong = {lat:55.859970, lng: -4.252720};
 
-var mapOptions = {  // Set up the map options - this is GoMA LatLong
-		center: new google.maps.LatLng(55.859970,-4.252720),
-		mapTypeId: google.maps.MapTypeId.ROADMAP,
+	var map = new google.maps.Map(document.getElementById('googleMap'), {
 		zoom: 15,
+		center: myLatLong,
 		panControl: false,
 		mapTypeControl: false,
 		zoomControlOptions: {
-	  	style: google.maps.ZoomControlStyle.SMALL,
-		position: google.maps.ControlPosition.TOP_RIGHT
-  	},
-  };
-
-var venueMap;  // Map() draws a map
-
-    venueMap = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
-
-	var startPosition = new google.maps.Marker({   // Marker() draws a marker using values given
-		position: pinLocation,
-		map: venueMap,
-		title: "GoMA", //tooltip
-		//icon: "../img/baseline-person_pin_circle-24px.png"
+			style: google.maps.ZoomControlStyle.SMALL,
+			position: google.maps.ControlPosition.TOP_RIGHT
+			}
 	});
-
 }
-
-function loadScript() {
-
-	var script = document.createElement('script');     // Create <script> element
-	  script.src = 'http://maps.googleapis.com/maps/api/js?key=AIzaSyB6Uq77t94TAXz7Ldp1Ax7VRpn2mDP2dsQ&callback=mapOptions';
-	  document.body.appendChild(script);   // Add element to page
-
-};
-
-window.onload = loadScript;  // on load call loadScript()
