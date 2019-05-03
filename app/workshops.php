22c478b8210163db3dafb1a5a81b678d13f01705
Alex Rhind <mrsrhino2009@gmail.com>
2019-05-03 14:12:10 +0100

    Workshop HTML CSS in

--- app/workshops.php
+++ app/workshops.php
@@ -9,7 +9,7 @@
   ***** Opening intro *****
   =========================== -->
 
-  <section class="wrapperWorkshop" style="width: 100%; background-image: url(img/liam-martens-680583-unsplash.png)">
+  <section class="wrapperWorkshop" style="width: 100%; background-image: url(img/liam-martens.png)">
 
   <div class="bannerWorkshop">
       <h1 class="gallery"><strong>Artist-led workshops<br>
@@ -20,8 +20,11 @@
 
   </div>
 </section>
+
+<div><p class="offer"><strong>GoMA FRIENDS</strong> GET 10% OFF EVENTS, SHOP & PRIORITY BOOKING | <strong>GoMA FRIENDS</strong> GET 10% OFF EVENTS, SHOP & PRIORITY BOOKING | <strong>GoMA FRIENDS</strong> GET 10% OFF EVENTS, SHOP & PRIORITY BOOKING | <strong>GoMA FRIENDS</strong> GET 10% OFF EVENTS, SHOP & PRIORITY BOOKING |</p></div>
+
   <!--
-  Gallery row
+  Artist bookings
   -------------------------------->
 
   <section class="#">
@@ -29,53 +32,80 @@
     <ul class="collapsible">
 
   <li>
-    <div class="collapsible-header blue-grey">
 
-      <img src="img/galleryArtist1.png" alt="Alex Perez" class="exampleArtist">
+    <div class="collapsible-header smaltBlueRule">
 
-      <div><p><strong>Workshops with Marky D</strong><i class="material-icons">play_arrow</i><br>10 – 31 October 2020</p>
-      <p><strong>In conversation with Marky D</strong><i class="material-icons">play_arrow</i><br>1900 for 1930 8 October 2020</p>
+      <div class="grid-six">
+
+        <div class="box20">
+            <img src="img/galleryArtist3.png" alt="Alex Perez" class="exampleArtistWorkshop">
+              <div class="bookings"><p><strong>Book workshops with Marky D</strong><i class="material-icons">play_arrow</i><br>10 – 31 October 2020</p>
+              <p><strong>Book 'In conversation' with Marky D</strong><i class="material-icons">play_arrow</i><br>1900 for 1930 8 October 2020</p>
+              </div>
+          </div>
+          <div class="box21Quote">
+            <p class="workshopCollapsible">  <img src="img/quotes.png" alt="monster quotes">&nbsp;&nbsp;Review of the last event do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad veniam, quis…</p>
+          </div>
+      <!-- end grid-six -->
       </div>
+    <!-- close collapsible header -->
     </div>
 
-    <div class="collapsible-body">
+    <div class="collapsible-body smaltBlueBRule">
       <p>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</p>
     </div>
   <!-- end collapsible 1 -->
   </li>
 
   <li>
-    <div class="collapsible-header blue-grey">
+    <div class="collapsible-header roseRule">
 
-    <img src="img/galleryArtist2.png" alt="Alex Perez" class="exampleArtist">
+      <div class="grid-six">
 
-    <div><p><strong>Workshops with Marky D</strong><i class="material-icons">play_arrow</i><br>10 – 31 October 2020</p>
-      <p><strong>In conversation with Marky D</strong><i class="material-icons">play_arrow</i><br>1900 for 1930 8 October 2020</p>
+        <div class="box20">
+            <img src="img/galleryArtist2.png" alt="Alex Perez" class="exampleArtistWorkshop">
+              <div class="bookings"><p><strong>Book workshops with Morag Macleish</strong><i class="material-icons">play_arrow</i><br>10 – 31 October 2020</p>
+              <p><strong>Book 'In conversation' with Morag Macleish</strong><i class="material-icons">play_arrow</i><br>1900 for 1930 8 October 2020</p>
+              </div>
+          </div>
+          <div class="box21Quote">
+            <p class="workshopCollapsible">  <img src="img/quotes.png" alt="monster quotes">&nbsp;&nbsp;Review of the last event do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad veniam, quis…</p>
+          </div>
+      <!-- end grid-six -->
       </div>
+    <!-- close collapsible header -->
     </div>
 
-    <div class="collapsible-body">
+    <div class="collapsible-body roseBRule">
       <p>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</p>
     </div>
   <!-- end collapsible 2 -->
   </li>
 
   <li>
-    <div class="collapsible-header blue-grey">
+    <div class="collapsible-header purpleRule">
 
-    <img src="img/galleryArtist3.png" alt="Alex Perez" class="exampleArtist">
+      <div class="grid-six">
 
-    <div><p><strong>Workshops with Marky D</strong><i class="material-icons">play_arrow</i><br>10 – 31 October 2020</p>
-      <p><strong>In conversation with Marky D</strong><i class="material-icons">play_arrow</i><br>1900 for 1930 8 October 2020</p>
+        <div class="box20">
+            <img src="img/galleryArtist1.png" alt="Alex Perez" class="exampleArtistWorkshop">
+              <div class="bookings"><p><strong>Book workshops with Alina Szapocznikow</strong><i class="material-icons">play_arrow</i><br>10 – 31 October 2020</p>
+              <p><strong>Book 'In conversation' with Alina Szapocznikow</strong><i class="material-icons">play_arrow</i><br>1900 for 1930 8 October 2020</p>
+              </div>
+          </div>
+          <div class="box21Quote">
+            <p class="workshopCollapsible">  <img src="img/quotes.png" alt="monster quotes">&nbsp;&nbsp;Review of the last event do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad veniam, quis…</p>
+          </div>
+      <!-- end grid-six -->
       </div>
+    <!-- close collapsible header -->
     </div>
 
-    <div class="collapsible-body">
+    <div class="collapsible-body purpleBRule">
       <p>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</p>
     </div>
   <!-- end collapsible 3 -->
   </li>
-
 
 
 </ul>
@@ -83,7 +113,6 @@
 
   </section>
 
-  <div><p class="offer"><strong>GoMA FRIENDS</strong> GET 10% OFF EVENTS, SHOP & PRIORITY BOOKING | <strong>GoMA FRIENDS</strong> GET 10% OFF EVENTS, SHOP & PRIORITY BOOKING | <strong>GoMA FRIENDS</strong> GET 10% OFF EVENTS, SHOP & PRIORITY BOOKING | <strong>GoMA FRIENDS</strong> GET 10% OFF EVENTS, SHOP & PRIORITY BOOKING |</p></div>
 
 
   </main>
